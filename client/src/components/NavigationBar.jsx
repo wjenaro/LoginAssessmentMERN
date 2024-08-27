e2387fb26a4cc5321f181c5577fec4d950ee6feb
Jenaro Wachira <wjenaro@gmail.com>
2024-08-27 16:42:01 +0300

    header design

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -3,14 +3,15 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function NavigationBar() {
   return (
-    <Navbar className="bg-body-tertiary">
+    <Navbar className="">
       <Container>
-        <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+        <Navbar.Brand href="/">MERN Assessment</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
+          <span>USername</span>
+         <a>Login</a>
+         <a>Signup</a>
+         <a>Logout</a>
         </Navbar.Collapse>
       </Container>
     </Navbar>
